8e14ad2955de909640f0421e2027f466780bec02
ManojTGN <manoj.thunderviz@gmail.com>
2022-08-21 09:46:22 +0530

    commenting (2/2)

--- src/modules/ascii.py
+++ src/modules/ascii.py
@@ -1,6 +1,6 @@
 import urllib.request
-from PIL import Image,ImageFont,ImageDraw
 from modules.parameter import isUrl
+from PIL import Image,ImageFont,ImageDraw
 
 """
     Terminal progress bar
@@ -19,7 +19,10 @@ def printProgressBar (iteration:int, total:int, prefix:str = '', suffix:str = ''
         filledLength = int(length * iteration // total)
         bar = fill * filledLength + '-' * (length - filledLength)
 
+        # Print The Bar
         print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+        
+        # Print New Line on Complete
         if iteration == total: print()
 
 """
@@ -57,20 +60,47 @@ class AsciiArt:
     def __init__(self,OPTIONS) -> None:
         self.OPTIONS = OPTIONS
 
-
+    """
+        Convert each pixel to intensity value (0-255) 
+        and mapped to the rendermode's ascii characters
+        @params:
+            IMAGE     - Required  :  The Input Image (Image)
+    """
     def loadImageCharSet(self,IMAGE:Image.Image) -> list:
+
+        # List Of Strings with Ascii Characters
         CHARSET = []
+
+        # Looping Throught All The Pixels In The
+        # Image By Height And Width
         for x in range(IMAGE.height):
+
+            # Appending A Empty String And Looping Throught
+            # The (X axis) Of The Image And Appending The
+            # Values To This String
             CHARSET.append("")
             for y in range(IMAGE.width):
+
+                # Adding The AsciiArt Char To The Last String Of The List
+                # According To The Pixel Value (0-255)
                 CHARSET[-1] += self.RENDER_MODES[self.OPTIONS['renderMode']][
                     map_( 
                         IMAGE.getpixel((y,x)),0,255,0, len(self.RENDER_MODES[self.OPTIONS['renderMode']]) - 1
                     )
                 ]
+        
         return CHARSET
 
+    """
+        Resize The Image To The "RenderQuality" Specified By The User
+        In Percentage (0-100)
+        @params:
+            IMAGE     - Required  :  The Input Image (Image)
+    """
     def resizeToRenderQuality(self,IMAGE:Image.Image) -> Image.Image:
+
+        # Converting the percentage to the value and resizing 
+        # the image and returning it
         return IMAGE.resize( 
             ( int( 
                 (IMAGE.width/100)*self.OPTIONS['renderQuality']
@@ -80,41 +110,77 @@ class AsciiArt:
             ) ) 
         )
     
-    def makeImage(self,CHARSET:list,frameNumber:int = 1,totalFrames:int = 1) -> Image.Image:
-
+    """
+        Drawing/ Writing The Charset Into A Black Image
+        @params:
+            CHARSET     - Required  :  The Charset List That Returned From loadImageCharSet() function (List)
+    """
+    def makeImage(self,CHARSET:list) -> Image.Image:
+        
+        # Loading the JetbrainMono font else loading the
+        # system default font
         try:FONT = ImageFont.truetype("src\\asserts\\font.ttf", 16)
         except:FONT = ImageFont.load_default()
+
+        # Getting the width and height of a single character of the font
+        # and setting the kerning(TextSpacing)
         width,height = FONT.getbbox('A')[2:]
         kerning = 2
 
+        # Creating an image with the width and height
         IMAGE = Image.new('RGB',(width*len(CHARSET[0])*kerning,height*len(CHARSET)))
+
+        # Making The Image Drawable To Write Text In It
         DRAW = ImageDraw.Draw(IMAGE)
+
+        # Looping through the CHARSET
         for index,LINE in enumerate(CHARSET):
-            printProgressBar(index, len(CHARSET)-1, prefix = f'Frame[{frameNumber}/{totalFrames}]:', suffix = 'Complete', length = 15)
+
+            # Updating The Progress Bar
+            printProgressBar(index, len(CHARSET)-1, prefix = f'Rendering:', suffix = 'Complete', length = 15)
             
-            #DRAW.text((0, index*height),LINE,(255,255,255),font=FONT)
+            # Looping Through Each Character In The Line
+            # And Placing The Character With Kerning
             for idx,CHAR in enumerate(LINE):
                 DRAW.text((idx*width*kerning, index*height),CHAR,(255,255,255),font=FONT)
+            """ DRAW.text((0, index*height),LINE,(255,255,255),font=FONT) """
         
         return IMAGE
     
+    """
+        Start converting the image to the asciiart image
+    """
     def Start(self):
 
+        # Checking if the fileLocation is an url or not
         if(isUrl(self.OPTIONS['inputFileLocation'])):
+
+            # Downloading the image and saving it as tmp.png and loading
+            # the image then converting the image to GreyScale image
             try:
-                urllib.request.urlretrieve(self.OPTIONS['inputFileLocation'],'Input.png')
+                urllib.request.urlretrieve(self.OPTIONS['inputFileLocation'],'tmp.png')
                 IMAGE = Image.open('Input.png').convert('L')
             except:
                 print("Unable to download/open the image!")
                 return
+
+        # if not url then it must be a systempath
         else:
+
+            # loading the image and converting it to an greyscale image
             try:IMAGE = Image.open(self.OPTIONS['inputFileLocation']).convert('L')
             except:
                 print("Unable to open the image!")
                 return
 
+        # Resizing the image to the user specified renderquality(0-100)
         IMAGE = self.resizeToRenderQuality(IMAGE)
+
+        # converting the resized image to the Ascii Characters List 
         CHARSET = self.loadImageCharSet(IMAGE)
 
+        # Now making a new image with the charset made
         IMAGE = self.makeImage(CHARSET)
+
+        # Saving the image to the output location
         IMAGE.save(self.OPTIONS['outputFolderLocation']+"\\output.jpg",optimize=True,quality=self.OPTIONS['outputQuality'])
--- src/modules/parameter.py
+++ src/modules/parameter.py
@@ -2,13 +2,23 @@ import os
 import requests
 from os import path
 
+# To Store Parameter Error
 PARAMS_ERRROR = ''
 
+"""
+    To Check If It Is An System Directory/Path
+    @params:
+        location - Required  : Path-To-File/Folder (str)
+"""
 def isLocalPath(location:str) -> bool:
 
     return path.exists(location)
 
-
+"""
+    To Check If It Is An URL
+    @params:
+        url - Required  :  URL (str)
+"""
 def isUrl(url:str) -> bool:
 
     try:
@@ -16,62 +26,81 @@ def isUrl(url:str) -> bool:
         return r.status_code == requests.codes.ok
     except: return False
 
-
+"""
+    Get All Parameters From The
+    User In The Terminal
+"""
 def getParameter() -> list:
     
     PARAMETER = []
+
+    # Input File Location (no default value) 
     PARAMETER.append(input("Input File Location:"))
     
+    # Render Quality (default: 100)
     PARAMETER.append(input("RenderQuality[1-100] (100):"))
     if(PARAMETER[-1] == "" or PARAMETER[-1] == " "):PARAMETER[-1] = '100'
 
+    # RenderMode (default: 0)
     PARAMETER.append(input("RenderMode[0-3] (0):"))
     if(PARAMETER[-1] == "" or PARAMETER[-1] == " "):PARAMETER[-1] = '0'
 
+    # OutputQuality (default: 100)
     PARAMETER.append(input("OutputQuality[1-100] (100):"))
     if(PARAMETER[-1] == "" or PARAMETER[-1] == " "):PARAMETER[-1] = '100'
 
+    # Output Folder Location (current Dir '.')
     PARAMETER.append(input(f"Output Folder Location ({os.getcwd()}):"))
     if(PARAMETER[-1] == "" or PARAMETER[-1] == " "):PARAMETER[-1] = os.getcwd()
 
     return PARAMETER
 
-
+"""
+    Validating The Parameters
+    @params:
+        PARAMETER - Required  :  The Arguments Specified By The User (List)
+"""
 def isParamValid(PARAMETER:list) -> bool:
     global PARAMS_ERRROR
 
+    # Checking If The Input File Location Is An SystemPath Or URL
     if( len(PARAMETER) == 0 or (not isLocalPath(PARAMETER[0]) and not isUrl(PARAMETER[0])) ):
         PARAMS_ERRROR = 'Invalid InputFileLocation At 1st Parameter'
         return False
     if(len(PARAMETER) == 1):return True
 
-
+    # Checking If The RenderQuality Can Be Converted To Int And Ranged From 1-100
     if(not PARAMETER[1].isnumeric() and (int(PARAMETER[1]) >0 and int(PARAMETER[1]) <= 100) ):
         PARAMS_ERRROR = 'Invalid RenderQuality At 2nd Parameter'
         return False
     if(len(PARAMETER) == 2):return True
 
-
+    # Checking If The RenderMode Can Be Converted To Int And Ranged From 0-2
     if(not PARAMETER[2].isnumeric() and (int(PARAMETER[2]) >=0 and int(PARAMETER[2]) < 3) ):
         PARAMS_ERRROR = 'Invalid RenderMode At 3rd Parameter'
         return False
     if(len(PARAMETER) == 3):return True
 
-
+    # Checking If The OutputQuality Can Be Converted To Int And Ranged From 1-100
     if(not PARAMETER[3].isnumeric() and (int(PARAMETER[3]) >0 and int(PARAMETER[3]) <= 100)):
         PARAMS_ERRROR = 'Invalid OutputQuality At 4th Parameter'
         return False
     if(len(PARAMETER) == 4):return True
 
-
+    # Checking If The OutputFileLocation Is A Valid Path
     if(not isLocalPath(PARAMETER[4])):
         PARAMS_ERRROR = 'Invalid OutputFileLocation At 5th Parameter'
         return False
     return True
 
-
+"""
+    Making The Parameter List To A Dict
+    @params:
+        PARAMETER - Required  :  The Arguments Specified By The User (List)
+"""
 def getParameterDict(PARAMETER:list) -> dict:
 
+    # Making The Dict With Default Values
     OPTIONS = {
         'renderQuality':100,
         'outputQuality':100,
@@ -80,6 +109,7 @@ def getParameterDict(PARAMETER:list) -> dict:
         'outputFolderLocation':os.getcwd()
     }
 
+    # Storing The Parameters According To The Dict
     OPTIONS['inputFileLocation'] = PARAMETER[0]
     if(len(PARAMETER) >= 2):OPTIONS['renderQuality'] = int(PARAMETER[1])
     if(len(PARAMETER) >= 3):OPTIONS['renderMode'] = int(PARAMETER[2])
